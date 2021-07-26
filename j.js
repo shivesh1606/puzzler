0adac11d9b5266112970f1cf45c83acc48509f9a
DarK-VEnOM-16 <75152702+DarK-VEnOM-16@users.noreply.github.com>
2021-07-26 13:55:54 +0530

    V4
    Completion of level done

--- j.js
+++ j.js
@@ -1,90 +1,117 @@
 const arr=[[11,12,13,14],[21,22,23,24],[31,32,33,34],[41,42,43,44]]
 let cur_empty=15;
-function play() {   
+var once=true;
+var start=true;
+const divs=document.querySelectorAll('.col');
+const rows=document.querySelectorAll('.row');
+const btn=document.getElementById("rand");
+
+function play(str) {   
     let beepsound = new Audio(   
-'sound1.wav');   
+`${str}.wav`);   
     beepsound.play();   
 }   
-function error()
-{
-    let beepsound = new Audio(   
-        'sound2.wav');   
-            beepsound.play();   
-}
-function error2()
-{
-    let beepsound = new Audio(   
-        'error.wav');   
-            beepsound.play();   
-}
-function switch_innerText(div1,div2)
+
+
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+  }
+
+function switch_innerHTML(div1,div2)
 {
     let tdiv=document.createElement('div');
     tdiv.classList.add('col');
-    tdiv.innerText=div1.innerText;
-    div1.innerText=div2.innerText;
-    div2.innerText=tdiv.innerText;
+    tdiv.innerHTML=div1.innerHTML;
+    div1.innerHTML=div2.innerHTML;
+    div2.innerHTML=tdiv.innerHTML;
+    // For changing the no.
+    let str=div1.classList[1];
+    let str2=div2.classList[1];
+
+    div1.classList.replace(str,str2);
+    div2.classList.replace(str2,str);
+    //
 
 }
 function switch_empty(div1,div2) {
     let tdiv=document.createElement('div');
     tdiv.classList.add('col');
-    tdiv.innerText=div1.innerText;
-    div1.innerText=div2.innerText;
-    div2.innerText=tdiv.innerText;
+    tdiv.innerHTML=div1.innerHTML;
+    div1.innerHTML=div2.innerHTML;
+    div2.innerHTML=tdiv.innerHTML;
+    // For changing the no.
+    let str=div1.classList[1];
+    let str2=div2.classList[1];
+
+    div1.classList.replace(str,str2);
+    div2.classList.replace(str2,str);
+    //
+
+
     div1.classList.remove('current_empty');
     div2.classList.add('current_empty');
+    
+    
 }
 
-const divs=document.querySelectorAll('.col');
-const rows=document.querySelectorAll('.row');
-const btn=document.getElementById("rand");
+
 window.addEventListener("keydown",(el)=>
 {   
     
     try{
     switch (el.code)
     {
-        case "ArrowUp":
+        case "ArrowDown":
+        case  "KeyS":
             if(cur_empty-4>=0){
                 switch_empty(divs[cur_empty],divs[cur_empty-4]);
                 cur_empty=cur_empty-4;
-                play();
+                play("sound1");
                 }
             else
-            {error();}
+            {
+                play("sound2");
+                
+            }
 
         break;
-        case "ArrowDown":
+        case "ArrowUp" :
+        case "KeyW":
             if(cur_empty+4<=15)
             {
             console.log("ArrowDown");
             switch_empty(divs[cur_empty],divs[cur_empty+4]);
             cur_empty=cur_empty+4;
             
-            play();
+            play("sound1");
             }
             else
-            {error();}
+            {play("sound2");}
         break;
-        case "ArrowLeft":
+        case "ArrowRight":
+        case "KeyD":
             if(cur_empty%4!==0){
             console.log("ArrowDown");
             switch_empty(divs[cur_empty],divs[cur_empty-1]);
             cur_empty=cur_empty-1;
-            play();}
+            play("sound1");}
             else
-            {error();}
+            {play("sound2");}
         break;
-        case "ArrowRight":
+        case "ArrowLeft":
+        case "KeyA":
             if(cur_empty%4!==3){
             console.log("ArrowDown");
             switch_empty(divs[cur_empty],divs[cur_empty+1]);
             cur_empty=cur_empty+1;
-            play();
+            play("sound1");
             }
             else
-            {error();}
+            {play("sound2");}
         break;
         default:
             console.log("Unknwon key",el.code);
@@ -93,25 +120,60 @@ window.addEventListener("keydown",(el)=>
 
     }
     catch{
-        error2();
+        play("error");
     }
     
     console.log(` cur_empty=${cur_empty}`);
+    let flag=1;
+    for(let d=0;d<16;d++)
+    {   
+        if(divs[d].classList[1]!==`${d+1}`)
+            flag=0;
+    }
+    if(flag===1)
+        {
+            play("Win");
+            console.log("Ypu win~~");
+        }
+    
 })
 
 
 btn.addEventListener("click",()=>
 {
-
-    for(let i=0;i<15;i++)
-    {
-
-        let rand=generaateRand(15);
-        switch_innerText(divs[i],divs[rand]);
+    start=true;
+    if(once)
+    {  
+        for(let i=0;i<15;i++)
+        {
+            let rand=generaateRand(15);
+            switch_innerHTML(divs[i],divs[rand]);
+        }
+        once=false;
     }
-})
+    else
+    {
+        for(let i=0;i<15;i++)
+        {
+            let rand=generaateRand(15);
+            switch_innerHTML(divs[i],divs[rand]);
+        }
+        for(let f of divs)
+        {
+            
+            if(f.innerHTML==="\n            ")
+            {
+                switch_empty(f,divs[15]);
+            }
+            else
+            {
+                f.classList.contains('current_empty')? f.classList.remove('current_empty') :{};
+            }
+        }
+        cur_empty=15;
+    }
+},once)
 function generaateRand(n)
 {
     return(Math.floor(Math.random()*n))
 }
-
